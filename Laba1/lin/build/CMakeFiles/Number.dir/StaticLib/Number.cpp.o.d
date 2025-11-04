9f8a0d63b8cd681cf8dad9cc58341040a07aecb1
RADd2 <nikitaabramchik95@gmail.com>
2025-11-04 17:06:48 +0300

    commit

---
+++ Laba1/lin/build/CMakeFiles/Number.dir/StaticLib/Number.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/Number.dir/StaticLib/Number.cpp.o: \
+  /Users/nikita_abr/Documents/Programming/BSU/OperationSystem/BSU_OperationSystems/Laba1/lin/StaticLib/Number.cpp \
+  /Users/nikita_abr/Documents/Programming/BSU/OperationSystem/BSU_OperationSystems/Laba1/lin/StaticLib/Number.h
